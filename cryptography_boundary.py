bf7cdae4efed584de54b6f9ed52ccd634b11f642
Fire Chicken <firechickenproductivity@gmail.com>
2024-12-01 08:05:36 -0700

    Define cryptography boundary

---
+++ cryptography_boundary.py
@@ -0,0 +1,18 @@
+import cryptography
+
+def create_public_private_key_pair(public_key_name, private_key_name):
+    pass
+
+def load_public_private_key_pair(public_key_name, private_key_name):
+    pass
+
+BLOCK_SIZE = 64
+
+def create_symmetric_key():
+    pass
+
+def encrypt_data_using_public_key(data, key):
+    pass
+
+def encrypt_data_using_symmetric_key(data, key):
+    pass
