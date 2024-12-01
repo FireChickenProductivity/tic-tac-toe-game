3571b4c011e48553addb173fd2af5cbb19d8ee86
Fire Chicken <firechickenproductivity@gmail.com>
2024-12-01 12:03:25 -0700

    Add encryption and decryption functions

--- cryptography_boundary.py
+++ cryptography_boundary.py
@@ -1,6 +1,7 @@
-import cryptography
 from cryptography.hazmat.primitives.asymmetric import rsa
 from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.asymmetric import padding
 
 RSA_KEY_SIZE = 4096
 RSA_PUBLIC_EXPONENT = 65537
@@ -15,7 +16,9 @@ def load_private_key(name):
     return key
 
 def load_public_key(name):
-    pass
+    key_bytes = _read_bytes_at_path(name)
+    key = serialization.load_pem_public_key(key_bytes)
+    return key
 
 def _write_bytes_at_path(data, path):
     with open(path, "wb") as file:
@@ -37,15 +40,30 @@ def create_public_private_key_pair(public_key_name, private_key_name):
     return public_key, private_key
 
 def load_public_private_key_pair(public_key_name, private_key_name):
-    pass
+    public_key = load_public_key(public_key_name)
+    private_key = load_private_key(private_key_name)
+    return public_key, private_key
 
 BLOCK_SIZE = 64
 
 def create_symmetric_key():
     pass
 
+def _create_padding_algorithm():
+    return padding.OAEP(
+            mgf=padding.MGF1(algorithm=hashes.SHA256()),
+            algorithm=hashes.SHA256(),
+            label=None
+        )
+
 def encrypt_data_using_public_key(data, key):
-    pass
+    return key.encrypt(data, _create_padding_algorithm())
+
+def decrypt_data_using_private_key(data, key):
+    return key.decrypt(data, _create_padding_algorithm())
 
 def encrypt_data_using_symmetric_key(data, key):
+    pass
+
+def decrypt_data_using_symmetric_key(data, key):
     pass
